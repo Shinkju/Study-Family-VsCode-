e19ac8e22aa5ba071ad6dd27e32cc27c76a8fa4a
kim <dpttm2@gmail.com>
2022-11-16 16:04:59 +0900

    11.16 css

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,3 @@
-/* 다른 페이지 링크 설정
-1. react-router-dom install
-2. Link 컴포넌트 import
-3. Link 컴포넌트의 to 속성에 연결할 url 작성하여 텍스트 노드로 보여줄 텍스트를 설정
-*/
-
 import { Link } from 'react-router-dom';
 
 function Main() {
@@ -14,7 +8,8 @@ function Main() {
             <nav>
                 <Link to="/">HOME</Link>
                 <Link to="/about">소개</Link>
-                <Link to="/menu">수강신청</Link>
+                <Link to="/menu">메뉴목록</Link>
+                <Link to="/tac">수강신청</Link>
             </nav>
         </div>
     );

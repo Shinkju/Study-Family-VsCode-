e9f648a62fa689b21f59a428b1446e3cc5219d3e
ParkTaeIk <109053949+ParkTaeIk@users.noreply.github.com>
2022-12-08 14:15:10 +0900

    박태익

--- src/pages/Professor/ProfessorStudentList.js
+++ src/pages/Professor/ProfessorStudentList.js
@@ -1,7 +1,6 @@
 import ProfessorStudentListCSS from '../Student/MyPage.module.css';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { decodeJwt } from '../../utils/tokenUtils';
 import { callStudentListAPI, insertLectureEvalAPI } from '../../apis/ProfessorApiCalls';
 import { useNavigate, useParams } from 'react-router-dom';
 
@@ -9,16 +8,8 @@ function ProfessorStudentList() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const params = useParams();
-  const evals = useSelector((state) => state.professorReducer);
   const lectures = useSelector((state) => state.professorReducer);
-  const evalsList = evals.data;
-  const evalCode = params.evalCode;
   const lectureCode = params.lectureCode;
-  // const token = decodeJwt(window.localStorage.getItem('accessToken'));
-
-  const [modifyMode, setModifyMode] = useState(false);
-
-  const [form, setForm] = useState({});
 
   useEffect(() => {
     // if (token) {
@@ -30,49 +21,11 @@ function ProfessorStudentList() {
     // }
   }, []);
 
-  /* 입력 양식의 값 변경될 때 */
-  const onChangeHandler = (e) => {
-    setForm({
-      ...form,
-      [e.target.name] : e.target.value
-    });
-  }
+     // 강좌 리스트로 페이지 넘기기
+     const onClickPage = (lectureCode) => {
 
-  const onClickModifyHandler = () => {
-
-    setModifyMode(true);
-    setForm({
-      evalCode : lectures.evalCode,
-      evalMiddle : lectures.evalMiddle,
-      evalFinal : lectures.evalFinal,
-      evalTask : lectures.evalTask,
-      evalAttend : lectures.evalAttend,
-    });
-  }
-
-  console.log('[evalCode] : ', evalCode)
-  console.log('[evals] : ', evals)
-  console.log('[evalsList] : ', evalsList)
-
-  /* 개인 정보 저장 버튼 클릭 이벤트 */
-  const onClickUpdateHandler = () => {
-
-    const formData = new FormData();
-
-    formData.append("evalCode", form.evalCode);
-    formData.append("evalMiddle", form.evalMiddle);
-    formData.append("evalFinal", form.evalFinal);
-    formData.append("evalTask" , form.evalTask);
-    formData.append("evalAttend" , form.evalAttend);
-
-    dispatch(insertLectureEvalAPI({
-      form : formData
-    }));
-
-    // alert("강좌 평가 성공")
-    // navigate('/layout/studentMyPage', { replace : true });
-    // window.location.reload();
-  }
+      navigate(`/layout/professorLectureList`, { replace : false })
+    }
 
   return (
     <>
@@ -91,10 +44,6 @@ function ProfessorStudentList() {
               <th>학점</th>
               <th></th>
             </tr>
-              {/* <th className={ProfessorStudentListCSS.notice}>중간고사</th>
-              <th className={ProfessorStudentListCSS.notice}>기말고사</th>
-              <th className={ProfessorStudentListCSS.notice}>과제</th>
-              <th className={ProfessorStudentListCSS.notice}>출석</th> */}
           </thead>
           <tbody>
             
@@ -105,54 +54,21 @@ function ProfessorStudentList() {
                   <td>{lectures.student.department.departmentName || ''}</td>
                   <td>{lectures.lecture.lectureName || ''}</td>
 
-                  <td><input name = "evalMiddle" size = "4" maxlength = "3"
-                             onChange = { onChangeHandler }
-                             value = {( !modifyMode ? lectures.eval[0]?.evalMiddle : form.evalMiddle) || ''}
-                             readOnly = { modifyMode ? false : true }/></td>
+                  <td>{lectures.eval[0]?.evalFinal || ''}점</td>
 
-                  <td><input name = "evalFinal" size = "4" maxlength = "3"
-                             onChange = { onChangeHandler }
-                             value = {( !modifyMode ? lectures.eval[0]?.evalFinal : form.evalFinal) || ''}
-                             readOnly = { modifyMode ? false : true }/></td>
+                  <td>{lectures.eval[0]?.evalFinal || ''}점</td>
 
-                  <td><input name = "evalTask" size = "4" maxlength = "3"
-                             onChange = { onChangeHandler }
-                             value = {( !modifyMode ? lectures.eval[0]?.evalTask : form.evalTask) || ''}
-                             readOnly = { modifyMode ? false : true }/></td>
+                  <td>{lectures.eval[0]?.evalTask || ''}점</td>
 
-                  <td><input name = "evalAttend" size = "4" maxlength = "3"
-                             onChange = { onChangeHandler }
-                             value = {( !modifyMode ? lectures.eval[0]?.evalAttend : form.evalAttend) || ''}
-                             readOnly = { modifyMode ? false : true }/></td>
+                  <td>{lectures.eval[0]?.evalAttend || ''}점</td>
+
                   <td>{lectures.eval[0]?.evalGrade || ''}</td>
-                  {/* <td> */}
-                    {/* <button onClick={ () => onClickTableTr()}>평가하기</button> */}
-                {/* </td> */}
                 
                 </tr>
                 </>]))}
-                  {/* <td><input type = "text" size = "4" maxlength = "3" name = "evalMiddle"
-                            //  value = {( form.evalMiddle || '')}
-                             onChange = { onChangeHandler } /></td>
-
-                  <td><input type = "text" size = "4" maxlength = "3" name = "evalFinal"
-                            //  value = {( form.evalFinal || '')}
-                             onChange = { onChangeHandler } /></td>
-
-                  <td><input type = "text" size = "4" maxlength = "3" name = "evalTask"
-                            //  value = {( form.evalTask || '')}
-                             onChange = { onChangeHandler } /></td>
-
-                  <td><input type = "text" size = "4" maxlength = "3" name = "evalAttend"
-                            //  value = {( form.evalAttend || '')}
-                             onChange = { onChangeHandler } /></td> */}
-              
           </tbody>
         </table>
-        { !modifyMode && 
-        <button onClick={onClickModifyHandler}>평가</button> }
-        { modifyMode &&
-        <button onClick={onClickUpdateHandler}>평가완료</button> }
+        <button onClick = { () => onClickPage(lectures.lectureCode)}>돌아가기</button>
       </div>
     </>
   );
